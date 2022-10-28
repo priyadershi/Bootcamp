18f9e82aded044b32df17348aaa8e9c0e6237c03
Pankaj Priyadershi <47598634+priyadershi@users.noreply.github.com>
2022-10-28 23:40:48 +0530

    Questions(1-12)

---
+++ Assignment_1/Question_10.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int d,m,y;
+    printf("Enter Date(DD/MM/YYYY): ");
+    scanf("%d/%d/%d",&d,&m,&y);
+    printf("\nDay-%d, Month-%d, Year-%d",d,m,y);
+    return 0;
+}
