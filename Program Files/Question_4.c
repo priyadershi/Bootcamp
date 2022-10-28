c68d73de4bb686f35a7d4d32c7a7ecf0efbbe0d3
Pankaj Priyadershi <47598634+priyadershi@users.noreply.github.com>
2022-10-28 23:31:28 +0530

    Assignment_1

---
+++ Program Files/Question_4.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+
+int main()
+{
+    float r;
+    printf("Enter Radius of Circle: ");
+    scanf("%f",&r);
+    printf("Area of circle is %.2f having the Radius %.2f",(3.14*r*r),r);
+    return 0;
+}
